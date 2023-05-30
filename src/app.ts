0d4d29f40cd6be3dd49101345560d3407e98113d
Nirob Hasan <nirob427417@gmail.com>
2023-05-30 18:10:02 +0600

    eslit pretteir

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express();
+const app: Application = express()
 
 app.use(cors())
 
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //Testing
 app.get('/', async (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
-export default app;
+export default app
